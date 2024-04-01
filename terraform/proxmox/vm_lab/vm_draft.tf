4a46b76a2b9caef5e0bc91d7e43ad23069894b3b
Aaron Earles <aaronearles@gmail.com>
2024-03-31 23:58:05 -0600

    10s delay

--- terraform/proxmox/vm_lab/vm_draft.tf
+++ terraform/proxmox/vm_lab/vm_draft.tf
@@ -60,15 +60,18 @@ resource "proxmox_vm_qemu" "vm" {
 
 ################### ANSIBLE ######################
 
-resource "time_sleep" "wait_30_seconds" {
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_10_seconds" {
   depends_on = [proxmox_vm_qemu.vm]
 
-  create_duration = "30s"
+  create_duration = "10s"
 }
 
 # This resource will create (at least) 30 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
 
 resource "ansible_group" "lab" {
@@ -94,5 +97,5 @@ resource "ansible_host" "labhost" {
   # a list that looks like: [ element_1, element_2, ..., element_N ]
   # yaml_list = jsonencode(local.decoded_vault_yaml.a_list)
   #   }
-  depends_on = [time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
